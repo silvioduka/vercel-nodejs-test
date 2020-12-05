70e320844976a0d7f2d2b6ec07e2b511a75c28de
Silvio <silvio.duka@gmail.com>
2020-12-05 18:03:05 +0100

    some changes

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
   const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`);
+  response.status(200).send(`Hello, ${name}!`);
 }
